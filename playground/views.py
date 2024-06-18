9f658ebc9409d63c34f7d3879e2b1fa19781536c
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-06-19 00:06:41 +0530

    chore: Add launch.json for debugging Django application

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
+    x = 1
+    y = 2
     return render(request, 'hello.html', {'name': 'suraj' } )
 
