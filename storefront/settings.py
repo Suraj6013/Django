8079f9cdec5b1f26b0576aebd59bca171d5d1a3c
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-06-19 23:19:52 +0530

    chore: Add debug_toolbar middleware for debugging purposes

--- storefront/settings.py
+++ storefront/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'storefront.urls'
